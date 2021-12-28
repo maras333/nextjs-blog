d750a806fe7a2f3abcb0ccb5b52878ef4ee60573
Marek Czyż <Marek.Czyz@dreamlab.pl>
2021-12-28 12:00:56 +0100

    Mainpage update

--- pages/index.js
+++ pages/index.js
@@ -21,14 +21,14 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Marek and I love to spend time improving my skills in JS:*</p>
+        <p>Hello, I'm Marek and I love to spend time improving my skills in JS!!!</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Sample Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
